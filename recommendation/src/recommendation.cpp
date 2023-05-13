74c0939183a08ab2fdccf43d3fdbe29e72ea9ef6
Vazhid <vazhid2000@mail.ru>
2023-05-13 20:16:47 +0300

    r

--- recommendation/src/recommendation.cpp
+++ recommendation/src/recommendation.cpp
@@ -1,1 +1,9 @@
 #include "recommendation.hpp"
+
+void Recommendation::forms_to_users() {}
+
+void Recommendation::users_to_forms() {}
+
+void Recommendation::vectorize_profile() {}
+
+void Recommendation::recommend() {}
